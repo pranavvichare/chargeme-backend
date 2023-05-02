2cc6f3f08875480d5dd1a1057b6a85427b78f0cc
Ragini Patel <ragini.patel@tntra.io>
2023-05-02 14:26:20 +0530

    changes

--- src/main/java/com/example/chargemebackend/user/model/User.java
+++ src/main/java/com/example/chargemebackend/user/model/User.java
@@ -1,12 +1,12 @@
 package com.example.chargemebackend.user.model;
 
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import javax.persistence.*;
 import java.util.UUID;
-@Getter
-@Setter
+@Data
 @Entity
 @Table(name = "users")
 public class User {
